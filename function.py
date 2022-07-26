d2f8804026320f375f5bc2adfe78e23ae5f60999
Park Solme <solme713@naver.com>
2022-07-26 13:28:03 +0900

    펑션1연습

---
+++ function.py
@@ -0,0 +1,31 @@
+# function.py
+
+#1)함수를 정의
+def setvalue(newvalue):
+    x = newvalue
+    print("내부에서 출력:", x)
+
+#2)함수를 호출
+retvalue = setvalue(5)
+print(retvalue)
+
+#다중의 값을 리턴
+def swap(x,y):
+    return y,x
+
+#호출
+result = swap(3,4)
+print(result[0])
+print(result[1])
+
+#교집합 문자를 리턴하는 함수
+def intersect(prelist, postlist):
+    #지역변수
+    result = []
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+    return result
+
+#함수 호출
+print(intersect("HAM","SPAM"))
